3034bb070360ee9684235e2cd54d563bd455b8cc
pattlebass <49322676+pattlebass@users.noreply.github.com>
2020-09-25 21:03:38 +0300

    Add files

---
+++ Avatar/code/Global.gd
@@ -0,0 +1,29 @@
+extends Node
+
+var fire_colors = {"blue":Color.blue, "red":Color.red}
+var ability_timers = { # in seconds
+	"firebending":{
+		1:{
+			"basic":1,
+		},
+		
+		"lightning":{
+			1:4,
+		},
+	},
+	"airbending":{
+		1:{
+			"basic":1,
+		}
+	},
+	"waterbending":{
+		1:{
+			"basic":1,
+		}
+	},
+	"earthbending":{
+		1:{
+			"basic":1,
+		}
+	},
+}
--- Avatar/code/Player.gd
+++ Avatar/code/Player.gd
@@ -1,19 +1,30 @@
 extends KinematicBody2D
 
 # Movement
-var run_speed = 200
-var jump_speed = 400
+var run_speed = 250
+var jump_speed = 450
 var gravity = 1200
 
 var velocity = Vector2()
 var jumping = false
 
 # Other
-var fire_colors = {"blue":Color.blue, "red":Color.red}
-var abilities = {
-	"firebending":{"level":10, "color":fire_colors.blue, "lightning_level":3},
-	"waterbending":{"level":10, "bloodbending_level":3,},
-	}
+var bending_abilities = {
+	"firebending":{"level":5, "color":Global.fire_colors.blue, "lightning_level":3},
+	"airbending":{"level":5, "flight_level":3},
+	"waterbending":{"level":5, "bloodbending_level":3,"healing_level":3},
+	"earthbending":{"level":5, "metalbending_level":3, "lavabending_level":3},
+}
+
+var stats = {
+	"avatar_state_level": 3,
+	"can_redirect_lightning":true,
+	"bending_abilities":bending_abilities
+}
+
+
+func _ready():
+	print(stats)
 
 
 func get_input():
@@ -29,6 +40,7 @@ func get_input():
 		velocity.x += run_speed
 	if left:
 		velocity.x -= run_speed
+
 
 func _physics_process(delta):
 	get_input()
