6c0dca7d49f54e0153f99a4ba2022c500c18fef1
pattlebass <49322676+pattlebass@users.noreply.github.com>
2020-12-28 22:24:11 +0200

    Add files

--- Avatar/ui/Debug.gd
+++ Avatar/ui/Debug.gd
@@ -4,14 +4,6 @@ onready var main = get_parent().get_parent()
 onready var player = main.get_node("Player")
 
 
-func _ready():
-	# Signals
-	$GridContainer/Element.connect("pressed", self, "toggle_from_list", 
-	["player.current_element", ["fire", "air", "water", "earth"]])
-	$GridContainer/IsAvatar.connect("pressed", self, "toggle_from_list", 
-	["player.stats.is_avatar", [true, false]])
-	
-
 func _process(_delta):
 	# Set Labels
 	$GridContainer/FPS.text = "FPS: %s" % Engine.get_frames_per_second()
@@ -19,6 +11,7 @@ func _process(_delta):
 	$GridContainer/IsAvatar.text = "Is Avatar: %s" % player.stats.is_avatar
 	$GridContainer/Health.text = "Health: %s / %s" % [player.stats.current_health, player.stats.max_health]
 	$GridContainer/Energy.text = "Energy: %s / %s" % [player.stats.current_energy, player.stats.max_energy]
+	$GridContainer/FireColor.text = "Fire color: %s" % player.stats.bending_abilities.firebending.color
 	
 	# Other
 	if Input.is_action_just_pressed("right_click") and player.current_element == "water":
@@ -26,13 +19,20 @@ func _process(_delta):
 		water_drop.global_position = get_global_mouse_position()
 		main.get_node("water").add_child(water_drop)
 
-func toggle_from_list(_var_path, _list):
-	var variable = str2var(_var_path)
-	var current_index = _list.find(variable)
-	if current_index != _list.size() - 1:
-		var next_index = current_index + 1
-		variable = _list[next_index]
-#	print(_var_path)
-#	print(variable)
-#	print(player.stats.is_avatar)
-	print(player.get("stats.is_avatar"))
+func next_in_array(_array:Array, _current):
+	if not _array.find(_current)+1 > _array.size()-1:
+		return _array[_array.find(_current)+1]
+	else:
+		return _array[0]
+
+func _on_Element_pressed():
+	var values = ["fire", "water", "air", "earth"]
+	if player.current_element:
+		player.current_element = next_in_array(values, player.current_element)
+
+func _on_IsAvatar_pressed():
+	player.stats.is_avatar = !player.stats.is_avatar
+
+func _on_FireColor_pressed():
+	var values = ["red", "blue"]
+	player.stats.bending_abilities.firebending.color = next_in_array(values, player.stats.bending_abilities.firebending.color)
--- Avatar/ui/PopupElementsWheel.gd
+++ Avatar/ui/PopupElementsWheel.gd
@@ -1,8 +1,8 @@
 extends PopupDialog
 
-var selected_element = ""
 onready var main = get_parent().get_parent()
 onready var player = main.get_node("Player")
+onready var selected_element = player.current_element
 
 
 func _input(_event):
@@ -11,7 +11,7 @@ func _input(_event):
 	
 	if Input.is_action_pressed("element_wheel"):
 		visible = true
-	else:
+	elif Input.is_action_just_released("element_wheel"):
 		visible = false
 		player.current_element = selected_element
 
@@ -19,14 +19,14 @@ func _input(_event):
 func _on_WaterLabel_mouse_entered():
 	selected_element = "water"
 	
-
 func _on_FireLabel_mouse_entered():
 	selected_element = "fire"
-
 
 func _on_EarthLabel_mouse_entered():
 	selected_element = "earth"
 
-
 func _on_AirLabel_mouse_entered():
 	selected_element = "air"
+
+func _on_FireColor_pressed():
+	pass # Replace with function body.
